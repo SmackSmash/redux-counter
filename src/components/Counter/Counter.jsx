3a9e519ca6944ee1c537c5ec16ba216476c57815
Dan Moffat <danmoffat@gmail.com>
2024-03-20 21:14:29 +0000

    Removed all counter state out of Counter component

--- src/components/Counter/Counter.jsx
+++ src/components/Counter/Counter.jsx
@@ -2,26 +2,18 @@ import { useState } from 'react';
 import './Counter.css';
 
 const Counter = () => {
-  const [count, setCount] = useState(0);
   const [addition, setAddition] = useState(0);
-
-  const handleDecrement = () => setCount(count - 1);
-
-  const handleIncrement = () => setCount(count + 1);
-
-  const handleReset = () => setCount(0);
 
   const handleSubmit = e => {
     e.preventDefault();
-    setCount(count + Number(addition));
   };
 
   return (
     <div className='counter'>
-      <h1>{count}</h1>
-      <button onClick={handleDecrement}>Decrement</button>
-      <button onClick={handleIncrement}>Increment</button>
-      <button onClick={handleReset}>Reset</button>
+      <h1>{}</h1>
+      <button onClick=''>Decrement</button>
+      <button onClick=''>Increment</button>
+      <button onClick=''>Reset</button>
       <form onSubmit={handleSubmit}>
         <input type='number' value={addition} onChange={e => setAddition(e.target.value)} />
         <button type='submit'>Add</button>
