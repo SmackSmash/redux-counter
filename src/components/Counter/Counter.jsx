e34da495d8f6a65c12abd041eb5cbb2c2f03c668
Dan Moffat <danmoffat@gmail.com>
2024-03-20 20:38:51 +0000

    Added npm start method and clear counter button

--- src/components/Counter/Counter.jsx
+++ src/components/Counter/Counter.jsx
@@ -9,6 +9,8 @@ const Counter = () => {
 
   const handleIncrement = () => setCount(count + 1);
 
+  const handleReset = () => setCount(0);
+
   const handleSubmit = e => {
     e.preventDefault();
     setCount(count + Number(addition));
@@ -19,6 +21,7 @@ const Counter = () => {
       <h1>{count}</h1>
       <button onClick={handleDecrement}>Decrement</button>
       <button onClick={handleIncrement}>Increment</button>
+      <button onClick={handleReset}>Reset</button>
       <form onSubmit={handleSubmit}>
         <input type='number' value={addition} onChange={e => setAddition(e.target.value)} />
         <button type='submit'>Add</button>
